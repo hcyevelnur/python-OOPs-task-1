9913f70a952da0ac38606236d40c71da124c69fa
Haciyev Elnur <hcyevelnur@Haciyevs-MacBook-Air.local>
2023-02-15 18:05:37 +0400

    update

---
+++ not-oop.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+class elanqoyy:
+    def __init__(self):
+        self.salam = []
+
+    def eylansaxla(self):
+        name = input("Marka: ")
+        tipi = input("Növü: ")
+        batarya = input("Batarya: ")
+        color = input("Rəngi: ")
+        kamera = input("Əsas Kamerası: ")
+        ram = input("RAM: ")
+        yaddas = input("Yaddaşı: ")
+        prosessor = input("Prasessor: ")
+        qiymet = input("Qiyməti: ")
+        try:
+         with open(input("Yaradılacaq not adını daxil et: "),"x") as file:
+            file.write(f"Markası: {name}\nNövü: {tipi}\nBataryası: {batarya}\nRəngi: {color}\nKamerası: {kamera}\nRAM: {ram}\nYaddaşı: {yaddas}\nPrasessor: {prosessor}\nQiyməti: {qiymet}\n------------------------\n")
+
+            print("Not yaradıldı")
+            elanqoy2 = {"marka": name, "növü": tipi, "batarya": batarya, "rəngi": color, "kamera": kamera, "ram": ram, "yaddas": yaddas, "prosessor": prosessor, "qiymeti": qiymet,}
+            self.salam.append(elanqoy2)
+            print("Elanınız saxlanıldı!")
+        except:
+            print("Bu adlı not var!")
+            print("Not adı eyni olduğu üçün elanınız saxlanılmadı!")  
+
+elan = elanqoyy()
+elan.eylansaxla()
+
+
